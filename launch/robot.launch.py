d8fbe916e997fe437083ac10e2f0496092c8cab0
Ashraf AlAssi <smart.teen.12345@gmail.com>
2024-07-07 11:30:16 +0300

    added launch file for real robot

---
+++ launch/robot.launch.py
@@ -0,0 +1,56 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+
+# Specify the name of the package
+pkg_name = "bicycle_sim"
+pkg_share_directory = get_package_share_directory(pkg_name)
+
+
+def generate_launch_description():
+    # Use xacro to process the file
+    urdf_path = "urdf/bicycle.urdf.xacro"
+    xacro_file = os.path.join(pkg_share_directory, urdf_path)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    # rsp
+    rsp_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {"robot_description": robot_description_raw},
+            {"use_sim_time": True},
+        ],
+    )
+
+    # TODO: Add the controller manager node here, and pass the URDF.
+    # Then pass tha .yaml file.
+
+    # joint_broad controller
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    # bicycle_steering_cont controller
+    bicycle_steering_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["bicycle_steering_cont"],
+    )
+
+    # Run the nodes
+    return LaunchDescription(
+        [
+            rsp_node,
+            joint_broad_spawner,
+            bicycle_steering_controller_spawner,
+        ]
+    )
